81f8074d9644b2329843a1872f49c0cce22c9b4a
Caleb Mech <cmech1@gmail.com>
2018-05-14 22:05:58 -0400

    Initial commit

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
 
 class Course extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      courses: []
+    }
+  }
+
+  fetchBooks() {
+    fetch("/api/courses/"+this.props.match.params.courseId)
+      .then(res => res.json())
+      .then(courses => this.setState({courses}))
+  }
+
+  componentDidMount() {
+    this.fetchBooks()
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.match.params.courseId!==this.props.match.params.courseId) {
+      this.fetchBooks()
+    }
+  }
+
   render() {
     return (
-      <h2>Course {this.props.match.params.courseId}!</h2>
+      <section>
+        <h2>Course {this.props.match.params.courseId}!</h2>
+
+        <ul>
+          {this.state.courses.map(course => {
+            return <li key={course.id}>{course.title}: ${course.price}</li>
+          })}
+        </ul> 
+      </section>
     )
   }
 }
