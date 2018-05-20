fbaa67910e53f53b625793a76b49488d9bf196de
Caleb Mech <cmech1@gmail.com>
2018-05-19 23:41:32 -0400

    Styled Book route

--- client/src/routes/Book.js
+++ client/src/routes/Book.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import { handleFetchError, ErrorMessage } from '../components/ErrorMessage';
+import MessengerButton from '../components/MessengerButton';
+import TimeAgo from 'timeago-react'
+import Loading from '../components/Loading';
 
 class Book extends Component {
     constructor(props) {
@@ -44,22 +48,31 @@ class Book extends Component {
         let book = this.state.book
 
         if(this.state.loading)  {
-            return <h2>Loading</h2>
+            return <Loading />
         }
-        return (
-            <section className="container">
-                <h2 className="pageTitle">{book.title}</h2>
-
-                {this.state.error.display === true &&
-                    <ErrorMessage error={this.state.error} />
-                }
-
-                <p>Posted: {book.datePosted}</p>
-                <p>Price: ${book.price}</p>
-            </section>
-
-
-        )
+        if(this.state.error.display === true) {
+            return <ErrorMessage error={this.state.error} />
+        } else {
+            return (
+                <section className="container">
+                    <h2 className="pageTitle">{book.title}</h2>
+                        <div className="row">
+                            <div className="col-md-4">
+                                <div className="card"><img className="card-img-top" src="/images/book3.jpg" alt="Textbook"/></div>
+                            </div>
+                            <div className="col-md-8">
+                                <p><span className="display-4 mb-5">${book.price}</span></p>
+                                
+                                <p className="m-0">Posted by <Link to={'user/'+book.seller.id}>{book.seller.name}</Link> <TimeAgo datetime={book.datePosted} locale='en' live={false} /></p>
+                                <div className="mb-4">
+                                    {book.courses.map(course => <Link to={"/course/"+course.id} key={course.id}><span className="badge badge-pill badge-primary ml-1">{course.code}</span></Link>)}
+                                </div>                                
+                                <MessengerButton id={book.seller.fb} />
+                            </div>
+                        </div>
+                </section>
+            )
+        }
     }
 }
 
