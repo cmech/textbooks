a39ec6b08828b776ec46952ac7c66c56a3b6cecc
Caleb Mech <cmech1@gmail.com>
2018-05-27 13:20:43 -0400

    Added ProfIleIcon component

---
+++ client/src/components/ProfileIcon.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './ProfileIcon.css'
+
+class ProfileIcon extends Component {
+    render() {
+        return (
+            <div className="profileIcon">
+                <img className="profilePicture" src={"https://graph.facebook.com/"+this.props.id+"/picture?type=large"} alt="User profile picture" />
+                {this.props.label !== false &&                 
+                <div className="profileLabel">
+                    <a href="/user/7">Caleb</a>
+                </div>
+                }
+            </div>
+        )
+    }
+}
+
+export default ProfileIcon
