a60901727d9d5d7c1f34a01b3ff76bb216d55fcd
Caleb Mech <cmech1@gmail.com>
2018-10-09 12:55:28 -0400

    More changes

--- client/src/components/ScrollToTop.js
+++ client/src/components/ScrollToTop.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
@@ -15,21 +14,3 @@ class ScrollToTop extends Component {
 }
 
 export default withRouter(ScrollToTop)
-=======
-import { Component } from 'react'
-import { withRouter } from 'react-router-dom'
-
-class ScrollToTop extends Component {
-  componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
-      window.scrollTo(0, 0)
-    }
-  }
-
-  render() {
-    return this.props.children
-  }
-}
-
-export default withRouter(ScrollToTop)
->>>>>>> 1924a4dbcf3480ddde1c442e6f989c392cee5dc8
--- client/src/functions.js
+++ client/src/functions.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export function handleGenericInputChange(e) {
   this.setState({
     [e.target.name]: e.target.value
@@ -10,16 +9,3 @@ export function Truncate(props) {
   let length = props.length
   return text.length > length ? text.substring(0, length - 3) + '...' : text
 }
-=======
-export function handleGenericInputChange(e) {
-  this.setState({
-    [e.target.name]: e.target.value
-  })
-}
-
-export function Truncate(props) {
-  let text = props.children
-  let length = props.length
-  return text.length > length ? text.substring(0, length - 3) + '...' : text
-}
->>>>>>> 1924a4dbcf3480ddde1c442e6f989c392cee5dc8
